3568fbc97e908f15919fbb6badb6b5779b83a661
DangamerYT <dangamergaming27@gmail.com>
2024-09-13 08:51:46 +0100

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["📺 SUBSCRIBE TO DANGAMERYT", "🌍 Monitoring Servers", "❓ Helping anyone!", "📆 24.7 Support!"];
+const statusTypes = [ 'online', 'idle', 'online', 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
