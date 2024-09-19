a0bd0c3cf8a98f0b3982871669ee445c54207943
DangamerYT <dangamergaming27@gmail.com>
2024-09-19 10:08:02 +0100

    index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["Watching all Discord servers"];
-const statusTypes = ['online'];
+const statusMessages = ["Watching all Discord servers", "Subscribe to DangamerYT];
+const statusTypes = ['online', 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
